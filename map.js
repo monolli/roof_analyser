5d6c902703a86ba2a2a3e61faab0d4e73c969b1e
Lucas Monteiro de Oliveira <lumonoli@hotmail.com>
2016-07-21 02:30:01 -0700

    geocode and initmap optimized, polygon drawing tool implemented

--- map.js
+++ map.js
@@ -1,108 +1,121 @@
-(function(window,mapster){
-  //load options
-	var options = mapster.MAP_OPTIONS,
-  
-  //create the element by getting the ID
-  element = document.getElementById('map');
-  
-  //create the actual map
-	var map = mapster.create(element, options);
-	var geocoder = new google.maps.Geocoder();
-	
-	document.getElementById('load-addresses').addEventListener('click', function() {
-	  geocodeAddress(geocoder, map);
-	});
-	
-  function geocodeAddress(geocoder,resultsMap) {
-	  var address = document.getElementById('address_text_area').value;
-	  geocoder.geocode({'address': address}, function(results, status) {
-	    if (status === google.maps.GeocoderStatus.OK) {
-	      options = {
-	        center: {
-	          lat: results[0].geometry.location.lat(),
-	          lng: results[0].geometry.location.lng()
-	        },
-	        zoom: 20,
-	        mapTypeId: google.maps.MapTypeId.SATELLITE, 
-	        minZoom: 4,
-	        zoomControlOptions: {
-	          position: google.maps.ControlPosition.LEFT_BOTTOM,
-	          style: google.maps.ZoomControlStyle.SMALL
-	        }
-	      };
-	      map = mapster.create(element, options);
-	    } else {
-	      alert('Geocode was not successful for the following reason: ' + status);
-	    }
-	  });
-  }
-	
-	
-	/*function getCoordinates(address,callback){
-	      var coordinates;
-	      geocoder.geocode({address: address},function (result,status){
-	        coordinates = results[0].geometry.location;
-	        callback(coordinates);
-	     })
-	      return coordinates;
-	    }
-	
-	map.getCoordinates('3025 Royal Street,Los Angeles,CA',function(coordinates){
-	  console.log(coordinates);
-	});
-	*/
-	
-	/*
-	
-  geocoder = new google.maps.Geocoder();
-  
-	
-	
-	function geocode(opts){
-	  geocoder.geocode({
-	    address:opts.address
-	  },function(results, status){
-	    if(status == google.maps.GeocoderStatus.OK){
-	      opts.success.call(this,result,status);
-	    }else{
-	      opts.error.call(this,status);
-	    }
-	  });
-	}
-	
-	geocode({
-	  address: '3025 Royal Street, Los Angeles, CA',
-	  success: function(results){
-	    var result = results[0];
-	    map.addMarker({
-	      lat: result.geometry.location.lat(),
-	      lng: result.geometry.location.lng(),
-	      draggable: true
-	    });
-	  },
-	  error: function(status){
-	    console.error(status);
-	  }
-	
-	})
-	*/
-	
-	/*//------------------------------------------------
-	
-	
-	
-	map._on('click',function(){
-	  alert('click');
-	  console.log(e);
-	  console.log(this);
-	});
-	
-	map._on('dragend',function(){
-	  alert('done dragging');
-	});
-	
-	
-	//------------------------------------------------*/
-	
-}(window, window.Mapster));
+/*
+Lucas Monteiro de Oliveira
+Summer 2016
+Tab == 4 spaces
+*/
+
+//Global variables
+var map;
+
+
+
+
+//#MAP_CREATION #START
+//Function: initMap()
+//Purpose:	create a standard map object
+//Parameters:	none
+//Returns:	nothing
+function initMap(){
+    //map customization goes here
+    var map_options = {
+    	center: {
+    	    lat: 34.020573,
+    	    lng: -118.285414
+    	},
+    	zoom: 15,
+    	mapTypeId: google.maps.MapTypeId.SATELLITE, 
+    	minZoom: 9,
+    	zoomControlOptions: {
+    	    position: google.maps.ControlPosition.LEFT_BOTTOM,
+    	    style: google.maps.ZoomControlStyle.SMALL
+    	}
+    }; 
+    //create the element by getting the ID
+    element = document.getElementById('map');
+    //create the map object
+    map = new google.maps.Map(element,map_options);
+}
+//#MAP_CREATION #END
+
+
+
+
+//#GEOCODE #START
+//create a google geocoder
+var geocoder = new google.maps.Geocoder();
+//Function: geocodeAddress(geocoder,resultMap)
+//Purpose:	show the map location according to the given address
+//Parameters:	the API geocoder and the map variable that is being used
+//Returns:	an alert if the status is != OK or the cropped map
+function geocodeAddress(geocoder,resultMap) {
+    var address = document.getElementById('address_text_area').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            //set the target coordinates and the zoom in the roof
+            lat2 = results[0].geometry.location.lat();
+            lng2 = results[0].geometry.location.lng();
+            
+            resultMap.setCenter({lat: lat2, lng: lng2});
+            resultMap.setZoom(20);
+            
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
+//#GEOCODE #END
+
+
+
+
+//#DRAWING_TOOL #START
+//Function: initDrawing()
+//Purpose:	initialize the drawing tool
+//Parameters:	none
+//Returns:	nothing
+function initDrawing(){
+    var polyOptions = {
+        fillColor: '#E58447', //orange
+        strokeColor: '#333333', //drak grey
+        strokeWeight: 1,
+        fillOpacity: 0.4,
+        editable: true
+    };
+// Creates a drawing manager for drawing polygons
+    drawingManager = new google.maps.drawing.DrawingManager({
+        //null == starts with dragging tool
+        drawingMode: google.maps.drawing.OverlayType.null,
+        drawingControlOptions: {    //show only the polygon tool
+            drawingModes: [
+                google.maps.drawing.OverlayType.POLYGON
+            ]
+        },
+        markerOptions: {
+            draggable: true
+        },
+        polylineOptions: {
+            editable: true
+        },
+        polygonOptions: polyOptions,
+        map: map
+    });
+}
+//#DRAWING_TOOL #END
+
+
+
+
+//                              Commands
+//----------------------------------------------------------------------------//
+
+//run the function and initialyze the map
+initMap();
+
+//run the function and initialyze the drawing tool/manager
+initDrawing();
+
+//create an action for the "Load Address" button
+document.getElementById('load-addresses').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+});
 
